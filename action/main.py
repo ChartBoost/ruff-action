6cbe846bf8b894d88588d7c5e079b9c74981ea76
brucearctor <5032356+brucearctor@users.noreply.github.com>
2023-04-02 19:57:17 -0700

    initial commit

---
+++ action/main.py
@@ -0,0 +1,25 @@
+"""GitHub Action for Ruff."""
+import os
+import re
+import shlex
+import sys
+from pathlib import Path
+from subprocess import run
+
+ACTION_PATH = Path(os.environ["GITHUB_ACTION_PATH"])
+ARGS = os.getenv("INPUT_ARGS", default="")
+SRC = os.getenv("INPUT_SRC", default="")
+VERSION = os.getenv("INPUT_VERSION", default="")
+
+version_specifier = ""
+if VERSION != "":
+    if not re.match(r"v?\d\.\d{1,3}\.\d{1,3}$", VERSION):
+        print("VERSION does not match expected pattern")
+        sys.exit(1)
+    version_specifier = f"=={VERSION}"
+
+req = f"ruff{version_specifier}"
+
+proc = run(["pipx", "run", req, *shlex.split(ARGS), *shlex.split(SRC)])
+
+sys.exit(proc.returncode)
