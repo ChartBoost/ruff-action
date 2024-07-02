42ca61cb2b3300938f210b51b1057ed5109f4e5c
David J. Felix <felix.davidj@gmail.com>
2024-07-02 14:53:43 -0500

    fix: handle edge case where changed files is empty

--- action/main.py
+++ action/main.py
@@ -12,6 +12,7 @@ ARGS = os.getenv("INPUT_ARGS", default="")
 SRC = os.getenv("INPUT_SRC", default="")
 VERSION = os.getenv("INPUT_VERSION", default="")
 CHANGED_FILES = os.getenv("CHANGED_FILES", "")
+IS_CHANGED_FILES_ENABLED = os.getenv("IS_CHANGED_FILES_ENABLED", default="false")
 
 version_specifier = ""
 if VERSION != "":
@@ -25,6 +26,11 @@ req = f"ruff{version_specifier}"
 # If CHANGED_FILES is not empty, split it into a list; otherwise, use SRC
 files_to_check = shlex.split(CHANGED_FILES or SRC)
 
+# If IS_CHANGED_FILES_ENABLED is true and CHANGED_FILES was empty, there were no files to check
+# Short circuit to prevent ruff from running on all files
+if IS_CHANGED_FILES_ENABLED is "true" and not CHANGED_FILES:
+    sys.exit(0)
+    
 proc = run(["pipx", "run", req, *shlex.split(ARGS), *files_to_check])
 
 sys.exit(proc.returncode)
